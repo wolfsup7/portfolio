d2cc0063459d1e1bb98e9e5aaaa72b6ae92a1dc4
wolfsup7 <laurendooley0324@gmail.com>
2022-11-04 17:27:53 -0400

    main page break

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ $(() => {
 
 
 //Event Listeners
-
+    // $(document).ready(function(){
+    //     $('#tblData tr').mouseover(function(){
+    //     $(this).addClass('hover');
+    //     });
+    //     $('#tblData tr').mouseout(function(){
+    //     $(this).removeClass('hover');
+    //     });
+    // });
 
 });
